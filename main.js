0912a5d3d547b6fc9846d0be16d8393e526cb37f
jt-ito <jtbjr07@gmail.com>
2026-05-12 20:49:04 -0600

    v1.19 - fix all title bar buttons and folder picker (sandbox: false, remove drag-region overlay)

--- main.js
+++ main.js
@@ -36,6 +36,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
+      sandbox: false,
       preload: path.join(__dirname, 'preload.js')
     },
     icon: path.join(__dirname, 'assets', 'icon.ico'),
